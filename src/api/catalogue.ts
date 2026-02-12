9211f5e44f06b8dad386770d3cfa86b9aabbd54a
Fergus Mullineux <fergmux@gmail.com>
2026-02-12 22:04:29 +0000

    stlye: lint fixes

--- src/api/catalogue.ts
+++ src/api/catalogue.ts
@@ -12,18 +12,18 @@ export const getCatalogue = async (): Promise<CatalogueItem[]> => {
   return [
     {
       code: RED_WIDGET_CODE,
-      name: "Red Widget",
+      name: 'Red Widget',
       price: RED_WIDGET_PRICE,
     },
     {
       code: GREEN_WIDGET_CODE,
-      name: "Green Widget",
+      name: 'Green Widget',
       price: GREEN_WIDGET_PRICE,
     },
     {
       code: BLUE_WIDGET_CODE,
-      name: "Blue Widget",
+      name: 'Blue Widget',
       price: BLUE_WIDGET_PRICE,
     },
-  ]
+  ];
 };
