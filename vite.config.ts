f14237ab8d0a33d1baa2100ab3548c4a0aef3ac3
Fergus Mullineux <fergmux@gmail.com>
2026-02-12 22:01:59 +0000

    chore: add import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 import { defineConfig } from 'vite';
 
 import tailwindcss from '@tailwindcss/vite';
@@ -6,4 +8,11 @@ import vue from '@vitejs/plugin-vue';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@composables': path.resolve(__dirname, 'src/composables'),
+      '@api': path.resolve(__dirname, 'src/api'),
+    },
+  },
+});
